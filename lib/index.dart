71e2e059a5c0d190c188847ecf3a8531d0671445
Christoph Waffler <waffler.christoph@gmail.com>
2020-06-16 21:20:28 +0200

    create index file for saving dependencies

---
+++ lib/index.dart
@@ -0,0 +1,9 @@
+// flutter sdk
+export 'package:flutter/material.dart';
+
+export 'package:youtube_downloader/drawer-swipebar.dart';
+export 'package:youtube_downloader/colors.dart';
+
+export 'dart:convert';
+
+
