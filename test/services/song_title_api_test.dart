cd1a44e37ee7c490eeaa88025578b66913dc3c13
Christoph Waffler <waffler.christoph@gmail.com>
2020-06-16 21:19:55 +0200

    Add test for API

---
+++ test/services/song_title_api_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:youtube_downloader/services/song_title_api.dart';
+
+void main() {
+  test('API for Song Metadata', () async {
+
+    final mmd = MusicMetadata();    
+
+    await mmd.search("Juice Wrld Lucid Dreams (Offical Music Video)");
+    print("Title: " + mmd.getTitleName());
+    print("Artist Name: " + mmd.getArtistName());
+    print("Album Name: "+ mmd.getAlbumName());
+  });
+}
