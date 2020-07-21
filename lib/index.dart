c0efa852c62cb363602d47885f52a2b3e32c95c1
CodingLegend27 <waffler.christoph@gmail.com>
2020-07-21 12:40:41 +0200

    Listing all imports

--- lib/index.dart
+++ lib/index.dart
@@ -3,7 +3,21 @@ export 'package:flutter/material.dart';
 
 export 'package:youtube_downloader/drawer-swipebar.dart';
 export 'package:youtube_downloader/colors.dart';
+export 'package:youtube_downloader/services/youtube_data.dart';
+export 'package:youtube_downloader/services/song_title_api.dart';
+export 'package:youtube_downloader/view_download_video_page.dart';
+export 'package:youtube_downloader/yt_search_results_view.dart';
+export 'package:youtube_downloader/services/downloader.dart';
 
 export 'dart:convert';
+export 'package:youtube_extractor/youtube_extractor.dart';
+export 'package:youtube_api/youtube_api.dart';
+export 'package:youtube_api_v3/youtube_api_v3.dart';
+export 'package:youtube_player_flutter/youtube_player_flutter.dart';
+// export 'package:flutter_downloader/flutter_downloader.dart';
 
+export 'package:downloads_path_provider/downloads_path_provider.dart';
+// export 'package:path/path.dart';
+export 'package:permission_handler/permission_handler.dart';
+// export 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
