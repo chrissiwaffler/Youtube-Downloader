46a364a143d0ad5763b1b84190c214b738b54d38
CodingLegend27 <waffler.christoph@gmail.com>
2020-07-21 12:38:14 +0200

    Update colors

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color background_app_color = Color.fromRGBO(243, 203, 203, 100);
+const Color homescreen_text_color = Color.fromRGBO(56, 72, 106, 100);
+const Color homescreen_inputfield_color = Color.fromRGBO(255, 255, 255, 100);
+
+const Color download_page_button_color = Color.fromRGBO(85, 63, 63, 100);
