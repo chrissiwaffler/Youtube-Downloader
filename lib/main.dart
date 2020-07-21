c0c14edee201403dcf2df2bf3b894685f39c6a83
CodingLegend27 <waffler.christoph@gmail.com>
2020-07-21 12:40:22 +0200

    Just changed to view the Homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:youtube_downloader/homescreen.dart';
+import 'package:youtube_downloader/index.dart';
+import 'package:youtube_downloader/view_download_video_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,24 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'YouTube Downloader',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Homepage(),
     );
   }
 }
